2ab226c8f5432e96e3b25e84196e51d3cddb6025
Mateo <mateo@hammr.com>
2024-08-07 22:25:13 +0200

    chore

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,7 @@ import { headerHeight } from "@/constants"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title:
-    "Sergio Gonzales | Chef profesional - Eventos - Catering - Chef en casa",
+  title: "Sergio Gonza | Chef profesional - Eventos - Catering - Chef en casa",
   description:
     "Chef profesional con experiencia en eventos, menu variado, asados, vegetariano, catering, chef en casa. Chef en casa basado en Manizales y disponible en todo el eje cafetero.",
 }
